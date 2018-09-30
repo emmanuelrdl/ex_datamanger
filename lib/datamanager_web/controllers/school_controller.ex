74c6a5933f15c28c34ef453c6a3a1288dff4be56
emmanuel rudelle <emmanuel.rudelle@hotmail.fr>
2018-09-30 19:40:52 +0200

    add bindPopup to markers

--- lib/datamanager_web/controllers/school_controller.ex
+++ lib/datamanager_web/controllers/school_controller.ex
@@ -4,10 +4,16 @@ defmodule DatamanagerWeb.SchoolController do
  import PhoenixGon.Controller
 
   def index(conn, _params) do
-    uri = "https://data.iledefrance.fr/api/records/1.0/search/?dataset=annuaire-de-leducation&rows=100"
-    response = HTTPotion.get uri, [timeout: 10_000]
-    {:ok, data } = response.body |> Poison.decode
-    conn = put_gon(conn, schools: data)
+    {:ok, data} = File.read("schools.json")
+    {:ok, schools}  = data |> Poison.decode
+    conn = put_gon(conn, schools: schools)
     render conn, "index.html"
+  end
+
+  def load_data do
+    uri = "https://data.iledefrance.fr/api/records/1.0/search/?dataset=annuaire-de-leducation&rows=10000"
+    response = HTTPotion.get uri, [timeout: 20_000]
+    {:ok, data } = response.body |> Poison.decode
+    File.write("schools.json", Poison.encode!(data), [:binary])
   end
 end
