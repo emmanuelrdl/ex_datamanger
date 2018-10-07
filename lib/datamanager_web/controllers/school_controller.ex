b38d68d9cab38c4ee634c68b01a5201e81171c15
emmanuel rudelle <emmanuel.rudelle@hotmail.fr>
2018-10-07 13:00:03 +0200

    add shools map with ES

--- lib/datamanager_web/controllers/school_controller.ex
+++ lib/datamanager_web/controllers/school_controller.ex
@@ -6,7 +6,7 @@ defmodule DatamanagerWeb.SchoolController do
   def index(conn, _params) do
     elastic_url = "http://localhost:9200"
     {:ok, data} = Elastix.Search.search(elastic_url, "schools", ["school"], %{ size: 10000})
-    schools  =  data.body["hits"] 
+    schools  =  Enum.map(data.body["hits"]["hits"], fn k -> k["_source"]["fields"] end)
     conn = put_gon(conn, schools: schools)
     render conn, "index.html"
   end
