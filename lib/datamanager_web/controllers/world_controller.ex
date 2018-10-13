8f74005ddb957bbaf9cb1ee7c82d6833445a5878
emmanuel rudelle <emmanuel.rudelle@hotmail.fr>
2018-10-13 18:12:43 +0200

    add module  DatamanagerWeb.PibManager

--- lib/datamanager_web/controllers/world_controller.ex
+++ lib/datamanager_web/controllers/world_controller.ex
@@ -2,28 +2,14 @@ require IEx
 defmodule DatamanagerWeb.WorldController do
  use DatamanagerWeb, :controller
  import PhoenixGon.Controller
+ import DatamanagerWeb.PibManager
 
   def index(conn, _params) do
     countries = get_countries
-    conn = put_gon(conn, countries: countries)
+    conn      = put_gon(conn, countries: countries)
     render conn, "index.html"
   end
 
-  def get_countries do
-    rows = File.stream!("pib.csv") |> CSV.decode |>  Enum.map(fn k -> k |> elem(1) end) |> Enum.drop(1)
-    countries = Enum.map rows, fn k ->
-      pib =  k |> Enum.at(-1)
-      if (k |> Enum.at(2) == "United States") do
-        country = Countriex.get_by(:name, "United States of America")
-      else
-        country = Countriex.get_by(:name, k |> Enum.at(2))
-      end
-      if country do
-        country |> Map.put(:pib, pib)
-      end
-    end
-    
-  end
 
 
 end
---
+++ lib/datamanager_web/services/pib_manager.ex
@@ -0,0 +1,18 @@
+defmodule DatamanagerWeb.PibManager do
+
+  def get_countries do
+    rows = File.stream!("pib.csv") |> CSV.decode |>  Enum.map(fn k -> k |> elem(1) end) |> Enum.drop(1)
+    countries = Enum.map rows, fn k ->
+      pib =  k |> Enum.at(-1)
+      if (k |> Enum.at(2) == "United States") do
+        country = Countriex.get_by(:name, "United States of America")
+      else
+        country = Countriex.get_by(:name, k |> Enum.at(2))
+      end
+      if country do
+        country |> Map.put(:pib, pib)
+      end
+    end
+  end
+
+end
